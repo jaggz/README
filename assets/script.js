f48717ca13a8e6539cde8a7239cb607961ea6c35
jaggz <js.dohil@gmail.com>
2022-07-17 18:25:48 -0700

    main

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,15 @@
-var topics = "HTML, CSS, Git, JavaScript";
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
 console.log(topics);
+console.log("shapes");
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+console.log(shapes[0]);
+// for loop
+console.log("for loop shapes");
+for(var x = 0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+   }
+console.log("for loop topics");
+for(var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+   }
